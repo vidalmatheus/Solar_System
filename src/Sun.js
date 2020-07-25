08ed41f5e86269aa9ba5c30b955494fe49e65ef4
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-07-25 03:20:58 -0300

    fixing sun's texture loading

--- src/Sun.js
+++ src/Sun.js
@@ -3,7 +3,7 @@ import { getSphere, getMaterial } from './utils.js';
 class Sun {
     constructor(scene, radius) {
         // Create the sun.
-        const sunTexture = new HREE.ImageUtils.loadTexture('/img/sun.jpg');
+        const sunTexture = new THREE.ImageUtils.loadTexture('/img/sun.jpg');
         const sunMaterial = getMaterial("basic", "rgb(255, 255, 255)", sunTexture);
         this.sun = getSphere(sunMaterial, radius);
         scene.add(this.sun);
